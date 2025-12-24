95649b9b090e2942dc1d6fca15acd9b6e1580c8b
teckydev <rs5916843@gmail.com>
2025-12-24 12:05:31 +0530

    fixed issuses

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,18 @@ import taskRoutes from "./routes/task.routes.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Health check
+
 app.get("/", (_req, res) => {
   res.status(200).json({ message: "API is running 🚀" });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
 
 import app from "./app.js";
 import connectDB from "./src/config/db.js";
